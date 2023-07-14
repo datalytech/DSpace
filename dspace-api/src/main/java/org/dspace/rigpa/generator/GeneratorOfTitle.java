8a27a427a029caaa460c6933f3c418e708b3b581
Vincenzo Mecca <vincenzo.mecca@4science.com>
2023-07-14 18:06:48 +0200

    [CST-11031] Fix & Refactor

--- dspace-api/src/main/java/org/dspace/rigpa/generator/GeneratorOfTitle.java
+++ dspace-api/src/main/java/org/dspace/rigpa/generator/GeneratorOfTitle.java
@@ -11,14 +11,22 @@ import org.apache.commons.lang3.StringUtils;
 import org.dspace.content.Item;
 import org.dspace.core.Context;
 import org.dspace.importer.external.metadatamapping.MetadataFieldConfig;
+import org.dspace.rigpa.consumer.RigpaGenerateTitleConsumer;
 
 /**
+ * This interface declares all the methods needed to generate
+ * a title with the consumer {@link RigpaGenerateTitleConsumer}
+ * 
  * @author Mykhaylo Boychuk (mykhaylo.boychuk@4science.com)
  */
 public interface GeneratorOfTitle {
 
-    public default String getDefaultSeparetor() {
+    public default String getDefaultSeparator() {
         return StringUtils.SPACE;
+    }
+
+    public default String getDefaultTitlePrefix() {
+        return "";
     }
 
     public String generateTitle(Context context, Item item);
--- dspace-api/src/main/java/org/dspace/rigpa/generator/SimpleGeneratorTitle.java
+++ dspace-api/src/main/java/org/dspace/rigpa/generator/SimpleGeneratorTitle.java
@@ -8,6 +8,8 @@
 package org.dspace.rigpa.generator;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.constraints.NotNull;
 
 import org.apache.commons.lang.StringUtils;
 import org.dspace.content.Item;
@@ -17,42 +19,54 @@ import org.dspace.importer.external.metadatamapping.MetadataFieldConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
+ * Simplest implementation of a {@link GeneratorOfTitle} bean,
+ * that will generate a title using a list of configured  {@link SimpleGeneratorTitle#fieldsToUseForGenerationOfTitle}:
+ * <ul>
+ *  <li> separated by a {@link SimpleGeneratorTitle#separator} and</li>
+ *  <li> optionally prefixed with {@link SimpleGeneratorTitle#titlePrefix}.</li>
+ * </ul>
+ * Once generated the value can be placed inside the {@link SimpleGeneratorTitle#targetMetadataFiled}.
+ * <br/>
+ * <br/>
+ * <b>You should have only one {@link SimpleGeneratorTitle} for each {@link SimpleGeneratorTitle#entityType}!</b>
+ *
  * @author Mykhaylo Boychuk (mykhaylo.boychuk@4science.com)
  */
 public class SimpleGeneratorTitle implements GeneratorOfTitle {
 
-    private String separetor;
+    @NotNull
     private String entityType;
-    private String titlePrefix;
+    @NotNull
     private MetadataFieldConfig targetMetadataFiled;
+
+    private String separator;
+    private String titlePrefix;
     private List<MetadataFieldConfig> fieldsToUseForGenerationOfTitle;
 
     @Autowired
     private ItemService itemService;
 
+    public SimpleGeneratorTitle() {
+        super();
+        this.titlePrefix = getDefaultTitlePrefix();
+        this.separator = getDefaultSeparator();
+    }
+
     @Override
     public String generateTitle(Context context, Item item) {
-        StringBuilder titleBuilder = new StringBuilder();
-        if (StringUtils.isNotBlank(this.titlePrefix)) {
-            titleBuilder.append(this.titlePrefix);
-        }
+        return fieldsToUseForGenerationOfTitle
+            .stream()
+            .map(md -> getMetadataFirstValue(item, md))
+            .filter(StringUtils::isNotBlank)
+            .collect(Collectors.joining(separator, titlePrefix, ""));
+    }
 
-        if (StringUtils.isBlank(this.separetor)) {
-            this.separetor = getDefaultSeparetor();
-        }
-
-        for (MetadataFieldConfig metadataFiled : fieldsToUseForGenerationOfTitle) {
-            var value = itemService.getMetadataFirstValue(item, metadataFiled.getSchema(),
-                                                                metadataFiled.getElement(),
-                                                                metadataFiled.getQualifier(), null);
-            if (StringUtils.isNotBlank(value)) {
-                if (StringUtils.isNotBlank(titleBuilder.toString())) {
-                    titleBuilder.append(this.separetor);
-                }
-                titleBuilder.append(value);
-            }
-        }
-        return titleBuilder.toString();
+    protected String getMetadataFirstValue(Item item, MetadataFieldConfig metadataFiled) {
+        return itemService.getMetadataFirstValue(
+            item, metadataFiled.getSchema(),
+            metadataFiled.getElement(),
+            metadataFiled.getQualifier(), Item.ANY
+        );
     }
 
     @Override
@@ -65,8 +79,8 @@ public class SimpleGeneratorTitle implements GeneratorOfTitle {
         return this.targetMetadataFiled;
     }
 
-    public void setSeparetor(String separetor) {
-        this.separetor = separetor;
+    public void setSeparator(String separator) {
+        this.separator = separator;
     }
 
     public void setEntityType(String entityType) {
@@ -83,6 +97,36 @@ public class SimpleGeneratorTitle implements GeneratorOfTitle {
 
     public void setFieldsToUseForGenerationOfTitle(List<MetadataFieldConfig> fieldsToUseForGenerationOfTitle) {
         this.fieldsToUseForGenerationOfTitle = fieldsToUseForGenerationOfTitle;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (entityType == null ? 0 : entityType.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        SimpleGeneratorTitle other = (SimpleGeneratorTitle) obj;
+        if (entityType == null) {
+            if (other.entityType != null) {
+                return false;
+            }
+        } else if (!entityType.equals(other.entityType)) {
+            return false;
+        }
+        return true;
     }
 
 }
