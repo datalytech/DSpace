120072fe614d71846289c7d93c99a28b546e1a44
Vincenzo Mecca <vincenzo.mecca@4science.com>
2024-05-02 15:20:42 +0200

    [#9505][#9476] Adds Optional and logs to SubmissionConfigServiceImpl for startup issues

--- dspace-api/src/main/java/org/dspace/submit/service/SubmissionConfigServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/submit/service/SubmissionConfigServiceImpl.java
@@ -9,6 +9,7 @@ package org.dspace.submit.service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 import org.dspace.app.util.SubmissionConfig;
 import org.dspace.app.util.SubmissionConfigReader;
@@ -16,6 +17,8 @@ import org.dspace.app.util.SubmissionConfigReaderException;
 import org.dspace.app.util.SubmissionStepConfig;
 import org.dspace.content.Collection;
 import org.dspace.core.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
@@ -25,56 +28,93 @@ import org.springframework.beans.factory.InitializingBean;
  */
 public class SubmissionConfigServiceImpl implements SubmissionConfigService, InitializingBean {
 
+    private static final Logger log = LoggerFactory.getLogger(SubmissionConfigServiceImpl.class);
+
     protected SubmissionConfigReader submissionConfigReader;
 
-    public SubmissionConfigServiceImpl () throws SubmissionConfigReaderException {
-        submissionConfigReader = new SubmissionConfigReader();
+    public SubmissionConfigServiceImpl () {
+        initSubmissionConfigReader();
+    }
+
+    protected Optional<SubmissionConfigReader> getSubmissionConfigReader() {
+        if (this.submissionConfigReader == null) {
+            initSubmissionConfigReader();
+        }
+        return Optional.ofNullable(submissionConfigReader);
+    }
+
+    private void initSubmissionConfigReader() {
+        try {
+            submissionConfigReader = new SubmissionConfigReader();
+        } catch (Exception e) {
+            log.error("Cannot initialize SubmissionConfigReader!", e);
+        }
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        submissionConfigReader.reload();
+        this.reload();
+    }
+
+    private void reload(SubmissionConfigReader submissionConfigReader1) {
+        try {
+            submissionConfigReader1.reload();
+        } catch (SubmissionConfigReaderException e) {
+            log.error("Cannot reload SubmissionConfigReader!", e);
+        }
     }
 
     @Override
     public void reload() throws SubmissionConfigReaderException {
-        submissionConfigReader.reload();
+        getSubmissionConfigReader().ifPresent(this::reload);
     }
 
     @Override
     public String getDefaultSubmissionConfigName() {
-        return submissionConfigReader.getDefaultSubmissionConfigName();
+        return getSubmissionConfigReader().map(s -> s.getDefaultSubmissionConfigName()).orElse(null);
     }
 
     @Override
     public List<SubmissionConfig> getAllSubmissionConfigs(Integer limit, Integer offset) {
-        return submissionConfigReader.getAllSubmissionConfigs(limit, offset);
+        return getSubmissionConfigReader().map(s -> s.getAllSubmissionConfigs(limit, offset)).orElse(List.of());
     }
 
     @Override
     public int countSubmissionConfigs() {
-        return submissionConfigReader.countSubmissionConfigs();
+        return getSubmissionConfigReader().map(s -> s.countSubmissionConfigs()).orElse(-1);
     }
 
     @Override
     public SubmissionConfig getSubmissionConfigByCollection(Collection collection) {
-        return submissionConfigReader.getSubmissionConfigByCollection(collection);
+        return getSubmissionConfigReader().map(s -> s.getSubmissionConfigByCollection(collection)).orElse(null);
     }
 
     @Override
     public SubmissionConfig getSubmissionConfigByName(String submitName) {
-        return submissionConfigReader.getSubmissionConfigByName(submitName);
+        return getSubmissionConfigReader().map(s -> s.getSubmissionConfigByName(submitName)).orElse(null);
     }
 
     @Override
     public SubmissionStepConfig getStepConfig(String stepID) throws SubmissionConfigReaderException {
-        return submissionConfigReader.getStepConfig(stepID);
+        return getSubmissionConfigReader().map(s -> {
+            try {
+                return s.getStepConfig(stepID);
+            } catch (SubmissionConfigReaderException e) {
+                throw new RuntimeException(e);
+            }
+        }).orElse(null);
     }
 
     @Override
     public List<Collection> getCollectionsBySubmissionConfig(Context context, String submitName)
             throws IllegalStateException, SQLException {
-        return submissionConfigReader.getCollectionsBySubmissionConfig(context, submitName);
+        return getSubmissionConfigReader().map(s -> {
+            try {
+                return getCollectionsBySubmissionConfig(context, submitName);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }).orElse(List.of());
     }
 
 }
